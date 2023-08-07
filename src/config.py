e4e8764ea968723cfe659888a7f4919852d329c3
ravil <nurgalievrav@gmail.com>
2023-08-07 08:19:49 +0300

    add new config loading

--- src/config.py
+++ src/config.py
@@ -1,21 +1,35 @@
-# состояние скважины (58 - действующая)
-sost_t = 58
+import json
 
-# жидкость, текущая по трубе
-agent = 106
 
-# размер дебита, максимальный и минимальный
-qg_max = 1000
-qg_min = 0
+class Config:
+    conf_path = "../input_data/config.json"
+    with open(conf_path, 'r', encoding='utf-8') as f:
+        data = json.loads(f.read())
 
-# вид оборудования, из которого выходит труба, 1 - скважина
-tipn = 1
+    if 'tipn' not in data['params']:
+        raise Exception('no config tipn provided')
 
-# записывать ли фактическое давление в таблицу (0 или 1)
-p_fak_write = 1
+    if 'agent' not in data['params']:
+        raise Exception('no config agent provided')
 
-# путь до файлов с данными - входных и выходных
+    if 'qg_max' not in data['params']:
+        raise Exception('no config qg_max provided')
+    if 'qg_min' not in data['params']:
+        raise Exception('no config qg_min provided')
 
-input_path = "../input_data/pipe_data.json"
+    if 'config_p_fak_write' not in data['params']:
+        raise Exception('no config p_fak_write provided')
 
-output_path = '../output_data/object_data.xlsx'
+    if 'input_path' not in data['params']:
+        raise Exception('no config input_path provided')
+    if 'output_path' not in data['params']:
+        raise Exception('no config output_path provided')
+
+    agent = data['params']['agent']
+    sost_t = data['params']['sost_t']
+    qg_max = data['params']['qg_max']
+    qg_min = data['params']['qg_min']
+    tipn = data['params']['tipn']
+    p_fak_write = data['params']['p_fak_write']
+    input_path = data['params']['input_path']
+    output_path = data['params']['output_path']
--- src/main.py
+++ src/main.py
@@ -1,28 +1,28 @@
 from file_reader import json_reader
 from file_writer import excel_write, zero_qg_writer
-from graph_creator import matplot_graph
-import config
-
-input_file_path = config.input_path
-output_file_path = config.output_path
+from graph_creator import matplot_graph, plotly_graph
+from config import Config
+import logging
 
 if __name__ == "__main__":
     # выполним чтение json файла и запишем данные в 3 списка -
     # список с именем и дебитом, id для объектов с 0 дебитом и список давлений
     table_list, qg0lst, p_fak_list = json_reader(
-        input_file_path,  # путь для входных данных
-        config.tipn,  # tipn параметр
-        config.sost_t,  # sost_t параметр
-        config.qg_min,  # параметр для min qg
-        config.qg_max,  # параметр для max qg
-        config.p_fak_write)  # записывать ли p_fak
+        Config.input_path,  # путь для входных данных
+        Config.tipn,  # tipn параметр
+        Config.sost_t,  # sost_t параметр
+        Config.qg_min,  # параметр для min qg
+        Config.qg_max,  # параметр для max qg
+        Config.p_fak_write)  # записывать ли p_fak
 
     # запишем id объектов с нулевым qg в отдельный файл
     zero_qg_writer(qg0lst)
 
-    excel_write(output_file_path,  # путь для выходных данных
-                table_list,  # список с id, cnt и qg объектов
-                p_fak_list)  # список с давлениями объектов
+    # excel_write(Config.output_path,  # путь для выходных данных
+    #         table_list,  # список с id, cnt и qg объектов
+    #         p_fak_list)  # список с давлениями объектов
 
     # построение графика с объектами и их дебитами
-    matplot_graph(table_list)
+
+    # matplot_graph(table_list) # matplot
+    plotly_graph(table_list)  # plotly
